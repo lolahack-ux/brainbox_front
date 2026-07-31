8b42fbbaf9407c46e50880e4881a06c09849cb4a
Lola Erhart <erhart.lola@hotmail.fr>
2026-07-31 14:46:57 +0200

    <feat> page d'acceuil et style retro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,14 @@ import { Connaissances } from './components/connaissances/connaissances';
 import { AjoutConnaissance } from './components/ajout-connaissance/ajout-connaissance';
 import { Assistant } from './components/assistant/assistant';
 import { ModificationConnaissance } from './components/modification-connaissance/modification-connaissance';
+import { Accueil } from './components/accueil/accueil';
 
 export const routes: Routes = [
+
+  {
+    path: '',
+    component: Accueil
+  },
   {
     path: 'connaissances',
     component: Connaissances
